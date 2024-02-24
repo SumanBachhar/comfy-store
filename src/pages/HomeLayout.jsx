726de3037fd3b4b7bc39f655634b4f74f06b0626
Suman-199 <suman.mmt199@gmail.com>
2024-02-25 01:18:08 +0530

    working on navbar

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,10 +1,15 @@
 import { Outlet } from "react-router-dom";
+import { Header } from "../components/Header";
+import { Navbar } from "../components/Navbar";
 
 export const HomeLayout = () => {
   return (
     <>
-      <nav>E-com</nav>
-      <Outlet />
+      <Header />
+      <Navbar />
+      <section className="align-element py-20">
+        <Outlet />
+      </section>
     </>
   );
 };
