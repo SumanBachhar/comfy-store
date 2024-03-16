018a6442d47cbc6d7c1872799fa5e04d052ae33f
Suman-199 <suman.mmt199@gmail.com>
2024-03-16 19:45:45 +0530

    update

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,3 +1,4 @@
+import { FeaturedProducts } from "../components/FeaturedProducts";
 import { Hero } from "../components/Hero";
 import { customFetch } from "../utils";
 const url = "/products?featured=true";
@@ -12,6 +13,7 @@ export const Landing = () => {
   return (
     <>
       <Hero />
+      <FeaturedProducts />
     </>
   );
 };
