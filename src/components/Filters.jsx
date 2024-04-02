ed49a330b4243fe39e20393adaa4aeeacd21a7f9
Suman-199 <suman.mmt199@gmail.com>
2024-04-02 14:29:06 +0530

    form update

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,7 +1,12 @@
-import { Form, Link } from "react-router-dom";
+import { Form, Link, useLoaderData } from "react-router-dom";
 import { FormInput } from "./FormInput";
+import { FormSelect } from "./FormSelect";
+import { FormRange } from "./FormRange";
+import { FormCheckbox } from "./FormCheckbox";
 
 export const Filters = () => {
+  const { meta, params } = useLoaderData();
+  // const { search, company, category, shipping, order, price } = params;
   return (
     <>
       <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4  gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
@@ -12,6 +17,44 @@ export const Filters = () => {
           name="search"
           size="input-sm"
           // defaultValue={search}
+        />
+        {/* CATEGORIES */}
+        <FormSelect
+          label="select category"
+          name="category"
+          list={meta.categories}
+          size="select-sm"
+          // defaultValue={category}
+        />
+        {/* CCOMPANIES */}
+        <FormSelect
+          label="select company"
+          name="company"
+          list={meta.companies}
+          size="select-sm"
+          // defaultValue={company}
+        />
+        {/* ORDER */}
+        <FormSelect
+          label="sort by"
+          name="order"
+          list={["a-z", "z-a", "high", "low"]}
+          size="select-sm"
+          // defaultValue={order}
+        />
+        {/* PRICE */}
+        <FormRange
+          name="price"
+          label="select price"
+          size="range-sm"
+          // price={price}
+        />
+        {/* SHIPPING */}
+        <FormCheckbox
+          name="shipping"
+          label="free shipping"
+          size="checkbox-sm"
+          // defaultValue={shipping}
         />
         {/* BUTTONS */}
         <button type="submit" className="btn btn-primary btn-sm">
---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+export const Loading = () => {
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center">
+        <span className="loading loading-ring loading-lg"></span>
+      </div>
+    </>
+  );
+};
