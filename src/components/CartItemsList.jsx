c1f9e5c26d3db30aa9f6136519598517538f670f
Suman-199 <suman.mmt199@gmail.com>
2024-04-20 08:50:31 +0530

    update

---
+++ src/components/CartItemsList.jsx
@@ -0,0 +1,13 @@
+import { useSelector } from "react-redux";
+import { CartItem } from "./CartItem";
+
+export const CartItemsList = () => {
+  const cartItems = useSelector((state) => state.cartState.cartItems);
+  return (
+    <>
+      {cartItems.map((item) => {
+        return <CartItem key={item.cartID} cartItem={item} />;
+      })}
+    </>
+  );
+};
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,3 +1,39 @@
+import { useSelector } from "react-redux";
+import { SectionTitle } from "../components/SectionTitle";
+import { CartItemsList } from "../components/CartItemsList";
+import { CartTotals } from "../components/CartTotals";
+import { Link } from "react-router-dom";
+
 export const Cart = () => {
-  return <div>Cart</div>;
+  const user = null;
+  const numItemsCart = useSelector((state) => state.cartState.numItemsCart);
+
+  if (numItemsCart === 0) {
+    return <SectionTitle text="Your cart is empty" />;
+  }
+  return (
+    <>
+      <SectionTitle text="Shopping Cart" />
+      <div className="mt-8 grid gap-8 lg:grid-cols-12">
+        <div className="lg:col-span-8">
+          <CartItemsList />
+        </div>
+        <div className="lg:col-span-4 lg:pl-4">
+          <CartTotals />
+          {user ? (
+            <Link to="/checkout" className="btn btn-primary btn-block mt-8">
+              proceed to checkout
+            </Link>
+          ) : (
+            <Link
+              to="/login"
+              className="btn btn-primary btn-block mt-8 uppercase"
+            >
+              please login
+            </Link>
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
